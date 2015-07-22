b5f3339844cd01623134e6762fc132c0c7374ac8
Corey Leveen <mntj11@gmail.com>
2015-07-21 21:48:46 -0400

    Name validation test

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "2.1.3"
+ruby "2.2.0"
 
 gem "activerecord", "~> 4.1.0"
 gem "rake"
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,7 @@ describe Person do
   it "requires a name to be valid" do
     # Write your spec here.
     # You can delete the pending line.
-    pending "Not written yet"
+    expect { Person.create! }.to raise_error
+    expect { Person.create!(first_name: 'George') }.to_not raise_error
   end
 end
